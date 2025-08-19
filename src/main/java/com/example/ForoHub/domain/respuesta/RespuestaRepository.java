e6574ab854209b7eddb901a3b1c30c6f3d58e332
fera1991 <00120520@uca.edu.sv>
2025-08-19 05:47:03 -0600

    Update: Actualizando Proyecto.

---
+++ src/main/java/com/example/ForoHub/domain/respuesta/RespuestaRepository.java
@@ -0,0 +1,6 @@
+package com.example.ForoHub.domain.respuesta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RespuestaRepository extends JpaRepository<Respuesta, Long> {
+}
---
+++ src/main/java/com/example/ForoHub/domain/topico/DatosActualizacionTopico.java
@@ -0,0 +1,7 @@
+package com.example.ForoHub.domain.topico;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DatosActualizacionTopico(@NotBlank String titulo, @NotBlank String mensaje, @NotNull Long curso) {
+}
